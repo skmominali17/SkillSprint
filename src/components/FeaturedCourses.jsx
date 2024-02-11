6cc74f0a841a245928a4df1a122d75de42153134
skmominali <skmominali17@gmail.com>
2024-02-12 01:55:48 +0530

    added course section

---
+++ src/components/FeaturedCourses.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Business from "../assests/images/Business.jpg";
+import Design from "../assests/images/Design.jpg";
+import Photography from "../assests/images/Photography.jpg";
+import Technology from "../assests/images/Technology.jpg";
+import { NavLink } from "react-router-dom";
+
+const FeaturedCourses = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Design",
+      img: Design,
+      link: "#",
+    },
+    {
+      id: 2,
+      title: "Technology",
+      img: Technology,
+      link: "#",
+    },
+    {
+      id: 3,
+      title: "Business",
+      img: Business,
+      link: "#",
+    },
+    {
+      id: 4,
+      title: "Photography",
+      img: Photography,
+      link: "#",
+    },
+  ];
+  return (
+    <div >
+      <h3 className="text-2xl font-medium text-white underline">Featured</h3>
+      {/* Card component */}
+      <div className="mx-auto container flex items-center justify-evenly mt-3">
+        {categories.map((category) => (
+          <NavLink to={category.link} key={category.id}>
+            <div className="flex flex-col items-center p-5 text-white hover:text-black hover:bg-green-400 rounded-lg transition duration-300 ease-in-out">
+              <div className="h-72 w-72 overflow-hidden rounded-lg">
+                <img
+                  src={category.img}
+                  alt={category.title}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-left text-lg mt-2 font-medium">
+                {category.title}
+              </h3>
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCourses;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { FaSearch } from "react-icons/fa";
 import SearchContext from "../contexts/SearchContext";
 import { databases, storage } from "../appwrite/Connection";
 import AuthContext from "../contexts/AuthContext";
+import CourseCategory from "../components/CourseCategory";
+import FeaturedCourses from "../components/FeaturedCourses";
 
 function Home() {
   const { searched } = useContext(SearchContext);
@@ -31,37 +33,45 @@ function Home() {
   }, [isLoggedIn]);
 
   return (
-    <div>
+    <div className="w-screen bg-gray-900">
       <div>
         <Navbar />
       </div>
       {/* Hero Image Section */}
-      <section className="relative h-screen w-screen bg-slate-500 flex justify-center">
-        <div className="relative h-2/3 w-3/5 mt-8 rounded-xl overflow-hidden">
-          <img
-            src="https://images.unsplash.com/photo-1497864149936-d3163f0c0f4b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="hero-image"
-            className="h-full w-full object-cover"
-          />
-          <div className="absolute top-52 right-28 text-white flex flex-col items-center rounded-lg">
-            <div className="font-semibold mb-4 uppercase text-6xl">
-              <span className="text-green-400">learn</span> what <br />
-              matters
-            </div>
-            <div className="flex items-center justify-center mt-2 w-full bg-green-400 rounded-lg p-2">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="w-full px-6 py-3 rounded-lg bg-zinc-700 text-white focus:outline-none text-lg"
-                onInput={(e) => searched(e)}
-              />
-              <button>
-                <FaSearch className="text-2xl mx-3 text-black" />
-              </button>
+      <div className="container mx-auto">
+        <section className="relative flex justify-center">
+          <div className="relative h-2/3 w-3/5 my-8 rounded-xl overflow-hidden">
+            <img
+              src="https://images.unsplash.com/photo-1497864149936-d3163f0c0f4b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="hero-image"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute top-52 right-28 text-white flex flex-col items-center rounded-lg">
+              <div className="font-semibold mb-4 uppercase text-6xl">
+                <span className="text-green-400">learn</span> what <br />
+                matters
+              </div>
+              <div className="flex items-center justify-center mt-2 w-full bg-green-400 rounded-lg p-2">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="w-full px-6 py-3 rounded-lg bg-zinc-700 text-white focus:outline-none text-lg"
+                  onInput={(e) => searched(e)}
+                />
+                <button>
+                  <FaSearch className="text-2xl mx-3 text-black" />
+                </button>
+              </div>
             </div>
           </div>
+        </section>
+        <div className="mt-2">
+          <CourseCategory />
         </div>
-      </section>
+        <div className="mt-8">
+          <FeaturedCourses />
+        </div>
+      </div>
     </div>
   );
 }
